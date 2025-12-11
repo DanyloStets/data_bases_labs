7a857d8870b9271701b937a826e3aa637b28a1d7
Ubuntu <Daniel@cloudLabs-VM.foeorqs4e54uxoc2fxc5cxp4dd.parx.internal.cloudapp.net>
2025-12-11 15:57:42 +0000

    Test deploy

--- DB lab5/app.py
+++ DB lab5/app.py
@@ -51,5 +51,5 @@ def protected():
     return jsonify({"message": "This is protected!"})
 
 if __name__ == '__main__':
-    app.run(debug=False, use_reloader=False)
+    app.run(debug=False, use_reloader=False, host="0.0.0.0", port=5000)
 
